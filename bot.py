5b2986ee9ba1fdd26b052ab58e67a4723df6f1ca
Nick North <daralf23@gmail.com>
2020-10-12 09:46:01 -0500

    QuoteBot : Added error reporting

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 # bot.py
 
-
 import os
 import random
 import logging
@@ -10,15 +9,30 @@ import json
 from dotenv import load_dotenv
 from discord.ext import commands
 
+
 # ###
 # Load Items
 # ###
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+ERRORCHANNEL = os.getenv('DISCORD_ERROR_CHANNEL')
 runDirectory = os.path.dirname(os.path.realpath(__file__))+"/"
-
+ErrChannel = None
 logging.basicConfig(level=logging.INFO)
+
+
+# ###
+# Functions
+# ###
+
+def get_error_channel():
+    guild = discord.utils.get(bot.guilds, name=GUILD)
+    for channel in guild.channels:
+        if(channel.name == ERRORCHANNEL):
+            tmpChannel = bot.get_channel(channel.id)
+            
+    return tmpChannel
 
 
 # ###
@@ -29,11 +43,14 @@ bot = commands.Bot(command_prefix='!')
 
 @bot.event
 async def on_ready():
-	guild = discord.utils.get(bot.guilds, name=GUILD)
-	print(
-		f'{bot.user.name} has connected to the following guild:\n'
-		f'{guild.name}(id: {guild.id})'
-	)
+    guild = discord.utils.get(bot.guilds, name=GUILD)
+    print(
+        f'{bot.user.name} has connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+    ErrChannel = get_error_channel()
+            
+    await ErrChannel.send("QuoteBot has Connected")
 
 @bot.event
 async def on_member_join(member):
@@ -42,40 +59,33 @@ async def on_member_join(member):
 		f'Hi {member.name}, welcome to Paper Handgrenades!'
 		)
 
-#@bot.command(name='donations', cls=participantID, help='Looks up most recent donation')
-#async def donations(ctx):
-#	url = "https://www.extra-life.org/api/participants/%s/donations" % participantID
-#	payload = {}
-#	headers = {}
-#	response = requests.request("GET", url, headers=headers, data = payload)
-
-#	await ctx.send(response)
-
 @bot.command(name='99', help='Responds with a a random quote from Brooklyn 99')
 async def nine_nine(ctx):
+    ErrChannel = get_error_channel()
     try:
         with open(runDirectory+'B99.json') as f:
             b99Options = json.load(f)
-        response = (random.choice(b99Options['responses']))['text']
+        await ctx.send((random.choice(b99Options['responses']))['text'])
     except Exception as e:
         print(e)
-        response = "Error Finding response from Brooklyn 99"
-    await ctx.send(response)
+        await ErrChannel.send("Error Finding response from Brooklyn 99")
+    
     
 @bot.command(name='8Ball', help='Responds with a a random selection from the Magic 8 Ball')
 async def eight_ball(ctx,question='8BALL'):
+    ErrChannel = get_error_channel()
     try:
         with open(runDirectory+'8Ball.json') as f:
             eightBallOptions = json.load(f)
         if(question=='8BALL' or question[-1] != '?'):
-            response = eightBallOptions['msgError']
+            await ctx.author.send(eightBallOptions['msgError'])
         else: 
-            response = (random.choice(eightBallOptions['responses']))['text']
+            await ctx.send((random.choice(eightBallOptions['responses']))['text']) 
     except Exception as e:
         print(e)
-    await ctx.send(response)    
+        await ErrChannel.send("Error Finding response from Magic 8 Ball")
+
     
-     
 # ###
 # Error Handling and Execution
 # ###
@@ -83,7 +93,7 @@ async def eight_ball(ctx,question='8BALL'):
 @bot.event
 async def on_command_error(ctx, error):
 	if isinstance(error, commands.errors.CommandNotFound):
-		await ctx.send('The command you entered does not exist')
+		await ctx.author.send('The command you entered does not exist')
 	else:
 		with open('err.log', 'a') as f:
 			f.write(f'Unhandled message: {error}\n')
